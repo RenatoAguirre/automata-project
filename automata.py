8b07ef8c806cbe3032ac8d67c1f7ba2e03b6fb43
RenatoAguirre <raaguirre1@miuandes.cl>
2024-04-26 18:59:05 -0400

    init

---
+++ automata.py
@@ -0,0 +1,20 @@
+class Automata:
+    # create an automata class
+    def __init__(
+        self,
+        states: list[str],
+        alphabet: list[str],
+        transitions: tuple,
+        end_state: str,
+        accept_states,
+    ):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.start_state = end_state
+        self.end_states = accept_states
+
+    def print_self(self):
+        print(
+            f"States: {self.states}, Alphabet: {self.alphabet}, Transitions: {self.transitions}, Start State: {self.start_state}, End States: {self.end_states}"
+        )
